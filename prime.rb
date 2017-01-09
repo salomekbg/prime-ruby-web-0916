bc81ad68f5993a7351fa1df2664154f1f5eb15b4
SluvsP <salomekbg@gmail.com>
2017-01-09 16:19:30 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,28 @@
-# Add  code here!
+# def prime?(num)
+# 	answer = 0
+# 	if num > 1
+# 		(2..(num - 1)).each do |x|
+# 			if num % x == 0
+# 				answer = false
+# 				break
+# 			else
+# 				answer = true
+# 			end
+# 		end
+# 	else
+# 		answer = false
+# 	end
+# 	answer
+# end
+
+def prime?(num)
+	answer = true
+	if num > 2
+		for x in (2..(num-1)).to_a
+			return answer = false if num % x == 0
+		end
+	elsif num < 2
+		answer = false
+	end
+	answer
+end
